08aca72b15fcb9298fd5f52cd0a4a8e652be6c4d
mblstore <bro.bahar@gmail.com>
2026-05-06 11:18:26 +0800

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -1,21 +1,25 @@
-// Fail ini akan diletakkan dalam folder /api/complete.js
 export default async function handler(req, res) {
-  const { paymentId, txid } = req.body;
-  const PI_API_KEY = "MASUKKAN_API_KEY_MAINNET_ANDA_DI_SINI";
+    if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
-  try {
-    const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/complete`, {
-      method: 'POST',
-      headers: { 
-        'Authorization': `Key ${PI_API_KEY}`,
-        'Content-Type': 'application/json' 
-      },
-      body: JSON.stringify({ txid })
-    });
+    const { paymentId, txid, network } = req.body;
+    
+    const PI_API_KEY = network === 'mainnet' 
+        ? "API_KEY_MAINNET" 
+        : "API_KEY_TESTNET";
 
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Gagal menyelesaikan bayaran" });
-  }
+    try {
+        const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/complete`, {
+            method: 'POST',
+            headers: { 
+                'Authorization': `Key ${PI_API_KEY}`,
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({ txid })
+        });
+
+        const data = await response.json();
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({ error: "Gagal Complete" });
+    }
 }
